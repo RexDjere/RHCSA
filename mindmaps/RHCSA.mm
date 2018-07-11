f1aabc9756f8a7d8fe51d640ab22006eff23edee
Rex Djere <rdjere@fedora.desktop>
2018-07-10 21:37:39 -0400

    Signed-off-by: Rex Djere <rdjere@fedora.desktop>

---
+++ mindmaps/RHCSA.mm
@@ -0,0 +1,13 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1531272880711" ID="ID_290260075" MODIFIED="1531272890250" TEXT="RHCSA">
+<node CREATED="1531272900533" ID="ID_599590570" MODIFIED="1531272903378" POSITION="right" TEXT="tasks">
+<node CREATED="1531272906662" ID="ID_492883234" MODIFIED="1531272921022" TEXT="understand and use essential tools">
+<node CREATED="1531272925081" ID="ID_658594013" MODIFIED="1531272929069" TEXT="files"/>
+<node CREATED="1531272932067" ID="ID_667493134" MODIFIED="1531272937528" TEXT="directories"/>
+<node CREATED="1531272947963" ID="ID_1439708016" MODIFIED="1531272960782" TEXT="command-line environments"/>
+<node CREATED="1531272973266" ID="ID_439012314" MODIFIED="1531272990882" TEXT="documentation"/>
+</node>
+</node>
+</node>
+</map>
